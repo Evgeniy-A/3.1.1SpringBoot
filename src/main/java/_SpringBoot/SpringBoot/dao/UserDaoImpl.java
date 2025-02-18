9faca12cdf956890660ca761385992f3f9243035
Evgeniy <Antonov.174.rus@mail.ru>
2025-02-18 20:56:07 +0500

    First commit

--- src/main/java/_SpringBoot/SpringBoot/dao/UserDaoImpl.java
+++ src/main/java/_SpringBoot/SpringBoot/dao/UserDaoImpl.java
@@ -1,11 +1,10 @@
 package _SpringBoot.SpringBoot.dao;
 
-
 import _SpringBoot.SpringBoot.models.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
@@ -15,25 +14,21 @@ public class UserDaoImpl implements UserDao {
     private EntityManager entityManager;
 
     @Override
-    @Transactional
     public void saveUser(String firstName, String lastName, String email) {
         entityManager.persist(new User(firstName, lastName, email));
     }
 
     @Override
-    @Transactional(readOnly = true)
     public User showUserById(int id) {
         return entityManager.find(User.class, id);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         return entityManager.createQuery("select u from User u", User.class).getResultList();
     }
 
     @Override
-    @Transactional
     public void updateUserById(int id, User updatedUser) {
         User userToBeUpdated = entityManager.find(User.class, id);
         userToBeUpdated.setFirstName(updatedUser.getFirstName());
@@ -42,8 +37,8 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
-    @Transactional
     public void removeUserById(int id) {
         entityManager.remove(entityManager.find(User.class, id));
     }
 }
+
--- src/main/java/_SpringBoot/SpringBoot/service/UserServiceImpl.java
+++ src/main/java/_SpringBoot/SpringBoot/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ package _SpringBoot.SpringBoot.service;
 import _SpringBoot.SpringBoot.dao.UserDao;
 import _SpringBoot.SpringBoot.models.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,26 +18,31 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void saveUser(String firstName, String lastName, String email) {
         userDao.saveUser(firstName, lastName, email);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User showUserById(int id) {
         return userDao.showUserById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
     @Override
+    @Transactional
     public void updateUserById(int id, User updatedUser) {
         userDao.updateUserById(id, updatedUser);
     }
 
     @Override
+    @Transactional
     public void removeUserById(int id) {
         userDao.removeUserById(id);
     }
